aebfa39c64f767a21c587caa8c82d56d21de5f59
Freffe <bergetf@gmail.com>
2021-04-13 12:09:13 +0200

    Update mapdata.sagas.js

--- client/src/redux/data/mapdata.sagas.js
+++ client/src/redux/data/mapdata.sagas.js
@@ -4,12 +4,6 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 import DataActionTypes from './mapdata.types';
 import { mapDataFetchSuccess, mapDataFetchFailure, mapDataFetchEmpty } from './mapdata.actions';
 
-// const APIKEY = "99b32e84-13f3-4e75-9c37-a123252afc54";
-/* ApiKey=99b32e84-13f3-4e75-9c37-a123252afc54
-Valt koordinatsystem=Google Mercator
-Srid=900913 */
-
-
 export function* mapDataFetch({ payload: { adress } }) {
     try {
         const mapData = yield axios.post("/fetchAdressData", {
